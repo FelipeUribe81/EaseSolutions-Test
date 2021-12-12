fd8a4bf97714cb0ca4b1dea5ed94f052ed261e76
FelipeUribe81 <felipe.uribe@correo.usa.edu.co>
2021-12-11 19:15:47 -0500

    Se documenta el código y se validan posibles mejoras para le código

--- code/app.py
+++ code/app.py
@@ -1,97 +1,103 @@
 # import numpy as np
-
-from os import path
-
+import sys
 
 class challenge:
-    def __init__(self) -> None:
-        self.n, self.m, self.map = self.read_file()
+    def __init__(self, file:str) -> None:
+        self.n, self.m, self.map = self.read_file(file)
         self.path = ''
-        # self.moves = {"up": [-1, 0], "down": [1, 0], "right": [0, 1], "left": [0, -1]}
-        # self.edge = {"up": 0, "down": self.n, "right": self.m, "left": 0}
-        self.moves = [0, 1, 0, -1, 1, 0, -1, 0]
+        """ 
+        The following variable is a representation of the movement matrix with the movements being:
+        Up: i + (j+1), Down: i + (j-1), Right: (i+1) + j, Left: (i-1) + j
+
+        The coefficient matrix would look like this:
+
+                    u   d  r   l
+        row 1 = i : [0, 0, 1, -1]
+        row 2 = j : [1, -1, 0, 0]
+
+        To save memory, the variable is represented as a one-dimensional vector.
+        """
+        self.moves = [0, 0, 1, -1, 1, -1, 0, 0]
         self.steps = 0
         self.drop = 0
         
 
-    def read_file(self):
-        input = open("../input/4x4.txt", "r")
+    def read_file(self, file:str) -> None:
+        input = open(f'../input/{file}', 'r')
         map = []
 
         for i, line in enumerate(input.readlines()):
-            row = []
             if i == 0:
                 n, m = line.strip("\n").split(" ")
             else:
-                [row.append(int(number)) for number in line.strip("\n").split(" ")]
+                """ 
+                A compressed list is used to read the rows and they are added to the list called map.
+                """
+                row = [int(number) for number in line.strip("\n").split(" ")]
                 map.append(row)
 
         return int(n), int(m), map
 
-    def movement(self, i, j, steps, current, path):
-        
+    def movement(self, i:int, j:int, steps:int, current:str, path:str):
+
+        """ 
+        To read the movements, a range is set corresponding to the number of elements over the total number of rows of the coefficient matrix 
+        over the total number of rows of the coefficient matrix.
+        """
         for move in range(len(self.moves)//2):
             isValid = i + self.moves[move] >= 0 and i + self.moves[move] < self.n and j + self.moves[move+len(self.moves)//2] >= 0 and j + self.moves[move+len(self.moves)//2] < self.m
             if (isValid and (self.map[i][j]> self.map[i + self.moves[move]][j + self.moves[move+len(self.moves)//2]])):
+                """
+                The element in the neighboring position is equated in the next variable being the neighbor up, 
+                down, right and left according to the iteration.
+                """
                 step = self.map[i + self.moves[move]][j + self.moves[move+len(self.moves)//2]]
+                """
+                The possible paths must be evaluated for each node that is visited, so the recursion is performed, and the visited neighbor 
+                must also be stored to know the traveled path.
+                """
                 self.movement(i + self.moves[move], j + self.moves[move+len(self.moves)//2], steps + 1, current, path + f' {step}')
 
+
         drop = current - self.map[i][j]
-        print(path.split(' '))
+        #print(path.split(' '))
+        """
+        It is validated if the number of visited is greater than the one set in the global variable, by default, it is set as 0
+        and the local number is initialized with 1. 
+        """
         if (steps > self.steps):
             self.steps = steps
             self.drop = drop
             self.path = path
 
+        """
+        If the global and local quantities coincide, the drop between the initial and final values must be checked.
+        """
         if (steps == self.steps and self.drop < drop):
             self.drop = drop
             self.path = path
 
-    # def movement(self, i, j, length, actual, path):
-    #     move_i = [0, 1, 0, -1]
-    #     move_j = [1, 0, -1, 0]
-    #     for k in range(4):
-    #         inside = i + move_i[k] >= 0 and i + move_i[k] < self.n and j + move_j[k] >= 0 and j + move_j[k] < self.m
-    #         # inside = (
-    #         #     (move == "up" and i + value[0] >= self.edge[move])
-    #         #     or (move == "down" and i + value[0] < self.edge[move])
-    #         #     or (move == "right" and j + value[1]) < self.edge[move]
-    #         #     or (move == "left" and j + value[1] >= self.edge[move])
-    #         # )
-    #         if inside and self.map[i][j] > self.map[i + move_i[k]][j + move_j[k]]:
-    #             # if ():
-    #             neighbor = self.map[i + move_i[k]][j + move_j[k]]
-    #             return self.movement(
-    #                 i + move_i[k], j + move_j[k], length + 1, actual, path + f'{neighbor}'
-    #             )
 
-    #     drop = actual - self.map[i][j]
-
-    #     if length > self.steps:
-    #         self.steps = length
-    #         self.drop = drop
-    #         self.path = path
-    #         # self.path = path
-    #         # self.drop = drop
-
-    #     if length == self.steps:
-    #         if self.drop < drop:
-    #             self.drop = drop
-    #             self.path = path
-    #             # self.drop = drop
-
-    def search(self):
+    def searchPath(self) -> None:
+        count = 0
+        """
+        We go through the matrix in order and one by one evaluating the correct path.
+        """
         for i in range(self.n):
             for j in range(self.m):
+                """
+                In order to avoid going through sections of previously seen roads and to save time and movements, we 
+                and to save time and movements, the position smaller than the current size of the path is omitted.
+                """
                 if (self.steps) < self.map[i][j]:
+                    #count += 1
                     self.movement(i, j, 1, self.map[i][j], f'{self.map[i][j]}')
         self.path = self.path.replace(' ', '-')
-
+        #print(count)
 
 if __name__ == "__main__":
-    app = challenge()
-    app.search()
-    print(app.path)
-    # print(app.drop)
-
-    #   print(app.n, app.m, app.map)
+    file = sys.argv[1]
+    print(file)
+    app = challenge(file)
+    app.searchPath()
+    print(f'The path is: {app.path}\nThe steps and drop are: {app.steps}, {app.drop}')
